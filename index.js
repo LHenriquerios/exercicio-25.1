93fd8f5e314bacb0755d73e31915aac4a6f9c0b5
LHenriquerios <henriquerios4@gmail.com>
2022-06-01 19:29:43 -0300

    feat: add variavel de ambiente

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Está vivo!!!'))
+app.get('/', (req, res) => res.send(`Está vivo e possui uma ${process.env.LET}`))
 app.listen(port, () => console.log(`Pai ta on na porta ${port}!`))
